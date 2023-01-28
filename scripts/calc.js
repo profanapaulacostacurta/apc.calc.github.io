0f73cda715df4e926ee92df7d62e6c1cee13b818
profanapaulacostacurta <123430271+profanapaulacostacurta@users.noreply.github.com>
2023-01-28 20:52:12 -0300

    Update calc.js

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,8 +1,8 @@
-import { soma, somaArray } from './modulos/soma.js';
+import { soma, somaArray } from "./modulos/soma.js";
 
 function testar(){
- var soma = "A soma de 3 + 5 é "+ soma(3,5);
- console.log("A soma de 3 + 5 é ", soma(3,5));
+ var resultado = "A soma de 3 + 5 é "+ soma(3,5);
+ console.log("A soma de 3 + 5 é ", resultado);
  document.getElementById("calculo").innerHTML= soma;
  console.log("sair");
  return;

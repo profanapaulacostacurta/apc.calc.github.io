7cbdca26c43d200669b912a45fc5b711fff7de44
profanapaulacostacurta <123430271+profanapaulacostacurta@users.noreply.github.com>
2023-01-28 20:15:24 -0300

    Update calc.js

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,5 @@
 import { soma, somaArray } from './modulos/soma.js';
 
 function testar(){
-  alert(somaArray([1,2,3,4,5]));
-  alert(soma(3,5));
+ document.getElementById("calculo").innerHTML= alert(soma(3,5);
 }

6935d0452272965cea990472b0ab74bfb9a7a44f
profanapaulacostacurta <123430271+profanapaulacostacurta@users.noreply.github.com>
2023-01-27 23:00:18 -0300

    Update soma.js

--- scripts/modulos/soma.js
+++ scripts/modulos/soma.js
@@ -1,1 +1,12 @@
-soma
+var somatorio = 0;
+function soma (primeiroNumero, segundoNumero){
+    return primeiroNumero + segundoNumero;
+}
+function somaArray(array){
+      var soma = 0;
+      for (let a of array){
+            soma = soma + a;
+      }
+      return soma;
+}
+export { somatorio, soma, somaArray }; // Exportação de todos os itens
